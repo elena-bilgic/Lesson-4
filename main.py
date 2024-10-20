dad35e9498eadb1fddeed0a96eb867047cbe7f73
elena-bilgic <elena.lipatnikova777@gmail.com>
2024-10-20 18:42:21 +0300

    set 2 removed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ set_cardealer1 = {"BMW 3 series", "BMW 5 series", "BMW ix", "BMW i8", "Range Rov
 
 set_cardealer3 = {"BMW 8", "Range Rover Sport SVR", "BMW x6"}
 
-set_union = set_cardealer1.union(set_cardealer2, set_cardealer3)
+set_union = set_cardealer1.union(set_cardealer3)
 print("Car selection after dealer merge: ", set_union)
 
 intersection = set_cardealer1.intersection(set_cardealer3)
