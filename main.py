b9d5f898a6609f4d644ff2e4d4f390a33e37e5df
elena-bilgic <elena.lipatnikova777@gmail.com>
2024-10-20 18:26:40 +0300

    set2 removed from the code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 set_cardealer1 = {"BMW 3 series", "BMW 5 series", "BMW ix", "BMW i8", "Range Rover Sport", "Range Rover Velar"}
-set_cardealer2 = {"BMW 7 series", "Porsche Panamera", "Audi R8", "BMW i8"}
+
 set_cardealer3 = {"BMW 8", "Range Rover Sport SVR", "BMW x6"}
 
 set_union = set_cardealer1.union(set_cardealer2, set_cardealer3)
