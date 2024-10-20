ac5c5062e3537021fd50ac9fde1243996f1abbfe
elena-bilgic <elena.lipatnikova777@gmail.com>
2024-10-20 18:32:08 +0300

    code adjusted

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 set_cardealer1 = {"BMW 3 series", "BMW 5 series", "BMW ix", "BMW i8", "Range Rover Sport", "Range Rover Velar"}
-
+set_cardealer2 = ["BMW 7 series", "BMW 5 series", "Range Rover Evoque", "Range Rover Velar"]
 set_cardealer3 = {"BMW 8", "Range Rover Sport SVR", "BMW x6"}
 
 set_union = set_cardealer1.union(set_cardealer2, set_cardealer3)
